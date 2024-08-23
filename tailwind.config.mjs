303ee312cc0ec1c7fb21a8f37c1fa696cda89144
Camille <devcclifton@gmail.com>
2024-08-23 10:33:15 -0500

    hero and marquee section added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,6 +16,20 @@ export default {
 				'subheading': [ 'Merriweather', 'serif'],
 				'body': ['DM Sans', 'sans-serif'],
 			},
+			animation: {
+				marquee: 'marquee 100s linear infinite',
+				marquee2: 'marquee2 100s linear infinite',
+			},
+			keyframes: {
+				marquee: {
+				'0%': { transform: 'translateX(0%)' },
+				'100%': { transform: 'translateX(-100%)' },
+				},
+				marquee2: {
+				'0%': { transform: 'translateX(100%)' },
+				'100%': { transform: 'translateX(0%)' },
+				},
+			},
 		},
 	},
 	plugins: [],
