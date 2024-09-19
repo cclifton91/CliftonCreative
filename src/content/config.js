211c0cce12b1499a90433dd88f0896924c745dcb
Camille <devcclifton@gmail.com>
2024-09-18 21:41:21 -0500

    -configured postpage

--- src/content/config.js
+++ src/content/config.js
@@ -1,19 +1,23 @@
-// 1. Import utilities from `astro:content`
+
 import { defineCollection, z } from 'astro:content';
 
-// 2. Define a `type` and `schema` for each collection
+
 const projects = defineCollection({
-  type: 'content', // v2.5.0 and later
+  type: 'content',
   schema: ({ image }) => 
     z.object({
         title: z.string(),
-        description: z.string(),
-        url: z.string().url(),
-        thumbnail: image(),
+        description: z.string().max(165, {
+        message: "Description cannot be longer than 165 characters",
+        }),
+        thumbnail: image().refine((img) => img.width >= 1000, {
+        message: "Image must be 1000px wide or more",
+        }),
+        isDraft: z.boolean().optional(),
   }),
 });
 
-// 3. Export a single `collections` object to register your collection(s)
+
 export const collections = {
     projects,
 };
