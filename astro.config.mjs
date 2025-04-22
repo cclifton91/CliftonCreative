1f14e9d088ec1a60a64c114cbfb8553ff63adb40
Camille <devcclifton@gmail.com>
2025-04-22 15:44:54 -0500

    added dependencies and got Nav up and working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import icon from 'astro-icon';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [alpinejs(), icon(), sitemap()]
+});
